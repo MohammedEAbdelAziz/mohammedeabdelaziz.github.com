65cae2d79d96275f29f19d70d9e058649ab2b6ab
Mohammed Essam <mohammedessamabdelaziz@outlook.com>
2025-12-29 19:43:38 +0200

    feat: added a post and 2 drafts

--- svelte.config.js
+++ svelte.config.js
@@ -2,10 +2,18 @@ import { mdsvex, escapeSvelte } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { createHighlighter } from 'shiki';
+import {
+	transformerNotationDiff,
+	transformerNotationHighlight,
+	transformerNotationFocus,
+	transformerNotationErrorLevel,
+	transformerRenderWhitespace,
+	transformerCompactLineOptions
+} from '@shikijs/transformers';
 
 const highlighter = await createHighlighter({
-	themes: ['github-dark'],
-	langs: ['javascript', 'typescript', 'svelte', 'html', 'css', 'json', 'bash', 'shell', 'sql', 'go']
+	themes: ['material-theme-palenight'],
+	langs: ['javascript', 'typescript', 'svelte', 'html', 'css', 'json', 'bash', 'shell', 'sql', 'go', 'mdx', 'yaml', 'ini', 'properties', 'dockerfile', 'docker', ]
 });
 
 /** @type {import('mdsvex').MdsvexOptions} */
@@ -19,7 +27,23 @@ const mdsvexOptions = {
 	},
 	highlight: {
 		highlighter: async (code, lang = 'text') => {
-			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'github-dark' }));
+			const html = escapeSvelte(
+				highlighter.codeToHtml(code, {
+					lang,
+					theme: 'material-theme-palenight',
+					transformers: [
+						transformerNotationDiff(),
+						transformerNotationHighlight(),
+						transformerNotationFocus(),
+						transformerNotationErrorLevel(),
+						transformerRenderWhitespace({ position: 'inline' }),
+						transformerCompactLineOptions()
+					],
+					meta: {
+						__raw: code
+					}
+				})
+			);
 			return `{@html \`${html}\`}`;
 		}
 	}
